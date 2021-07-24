4af962976eb7611b51d7e851ad0758b38b29d59c
Evelyn <evelyn.chavez92@yahoo.com>
2021-07-24 16:44:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = drivers.filter(driver => driver.slice(2, 4)) 
+console.log = drivers
+
+
+ 
