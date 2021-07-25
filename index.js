c2e0dadc3e2f291773655b93fa8d41741c0ab109
Evelyn <evelyn.chavez92@yahoo.com>
2021-07-24 20:12:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
-const findMatching = drivers.filter(driver => driver.slice(2, 4)) 
-console.log = drivers
 
+function findMatching (match, drivers) {
+    return match.filter( possibleMatch =>
+        possibleMatch.toLowerCase() === drivers.toLowerCase()
+
+        )
+}
+
+function fuzzyMatch (match, testString) {
+    return match.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+}
+
+function matchName(match, driversName) {
+    return match.filter ( record => record.name === driversName)
+}
+ 
 
  
