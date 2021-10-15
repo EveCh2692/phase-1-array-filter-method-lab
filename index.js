e2eb303727e3dcb3d8e311cd87bd96ac1ed7149c
Evelyn <evelyn.chavez92@yahoo.com>
2021-10-15 01:36:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-
-
-function findMatching (match, drivers) {
-    return match.filter( possibleMatch =>
-        possibleMatch.toLowerCase() === drivers.toLowerCase()
-
-        )
+const  findMatching = function (match, drivers){
+return match.filter(possibleMatch => possibleMatch.toLowerCase()=== drivers.toLowerCase())
 }
 
-function fuzzyMatch (match, testString) {
-    return match.filter( possibleMatch =>
-        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
-    )
+const fuzzyMatch = function (match, testString){
+return match.filter(possibleMatch =>
+    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0)
+
 }
 
-function matchName(match, driversName) {
-    return match.filter ( record => record.name === driversName)
+const matchName = function (match, driverName){
+return match.filter(record => record.name === driverName)
 }
- 
-
- 
